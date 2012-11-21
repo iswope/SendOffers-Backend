dd4f72ae774fda2fe8bc4f67297db0f655e9273a
SendOffers <iswope@sendoffers.com>
2012-11-21 15:50:45 +0000

    2012-11-21

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -4,126 +4,77 @@ require 'rexml/document'
 
 class AdsController < ApplicationController
   
-  
-  
-  def make_request(service_url, method, req_body)
-    #api_user = credentials[0] + "\\" + credentials[1]
-    api_user = 'SENDOFFE\admin'
-    passwd = 'Obl1skc3p0!'
-    accountId = "6a41fce9-8dbd-4464-a07b-7c87ed0001c6"
-    http = Net::HTTP.new('services.reachmail.net', 443)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE  
     
-    if method == 'GET'
-      req = Net::HTTP::Get.new(service_url)
-      req.basic_auth api_user, passwd
-      service_response = http.request(req)
-    elsif method == 'POST'
-      
-      listRequest = Net::HTTP::Post.new('/Rest/Contacts/v1/lists/query/'+accountId)
-      #listRequest = Net::HTTP::Post.new('/Rest/Reports/v1/mailings/query/' + accountId)
-      listRequest.basic_auth 'SENDOFFE\admin', 'Obl1skc3p0!'
-      listRequest["Content-Type"] = "text/xml"
-      #filterXML = '<ListFilter></ListFilter>'
-      #serviceResponse = http.request(listRequest, filterXML)
-      serviceResponse = http.request(listRequest)
-      
-      
-=begin      
-      req = Net::HTTP::Post.new(service_url)
-      req.basic_auth api_user, passwd
-      req.content_type = "texl/xml"
-      req.body = req_body
-      #service_response = http.start { |http| http.request req }
-      serviceResponse = http.request(req)
-=end      
-    end
-    
-    return service_response
-    
-  end
-  
-  
-  
   def get_lists_api
+    #report = params[:report]
     #@client = Client.find(:first, :conditions => ['uuid = ?', params[:id]])
     
-    #req_body = "<ListFilter></ListFilter>"
-    accountId = "6a41fce9-8dbd-4464-a07b-7c87ed0001c6"
+    report = "mailings"
     
-    #service_url= '/Rest/Reports/v1/mailings/query/'
-    #service_url = '/Rest/Contacts/v1/lists/query/'
+    case report
+      when "contacts"
+        service_url= '/Rest/Contacts/v1/lists/query/'
+        filter = '<ListFilter></ListFilter>'
+      when "mailings"
+        service_url= '/Rest/Reports/v1/mailings/query/'
+        filter = "<MailingReportFilter><MaxResults>10</MaxResults><ScheduledDeliveryOnOrAfter>2012-11-10T12:00:47</ScheduledDeliveryOnOrAfter><ScheduledDeliveryOnOrBefore>2012-11-19T12:00:47</ScheduledDeliveryOnOrBefore></MailingReportFilter>"
+      when "content"
+        service_url= '/Rest/Content/Mailings/v1/query/'
+        filter = '<MailingFilter><MaxResults>10</MaxResults></MailingFilter>'
+      when "mailing"
+        service_url= '/Rest/Reports/v1/mailings/'
+        filter = '<MailingReport></MailingReport>'
+    end
     
-    #@response = Array.new
-    #@response = make_request(service_url+accountId, 'POST', req_body)
-#=begin     
+    accountid = '6a41fce9-8dbd-4464-a07b-7c87ed0001c6'
     http = Net::HTTP.new('services.reachmail.net',443)
+    req = Net::HTTP::Post.new(service_url + accountid)
+    #filter = "<MailingReportFilter><MaxResults>10</MaxResults><ScheduledDeliveryOnOrAfter>2012-11-01T12:00:47</ScheduledDeliveryOnOrAfter><ScheduledDeliveryOnOrBefore>2012-11-19T12:00:47</ScheduledDeliveryOnOrBefore></MailingReportFilter>"
+    
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    
-    #accountIdRequest.basic_auth 'SENDOFFE\admin', 'Obl1skc3p0!'
-    #serviceResponse = http.request(accountIdRequest)
-    #doc = REXML::Document.new(serviceResponse.body)
-    #root = doc.root
-    #accountId = root.elements['AccountId'].text
-    #accountIdRequest = Net::HTTP::Get.new('/Rest/Administration/v1/users/current')
-    
-    #req = Net::HTTP::Post.new('/Rest/Contacts/v1/lists/query/' + accountId)
-    req = Net::HTTP::Post.new('/Rest/Reports/v1/mailings/query/' + accountId)
-    #req = Net::HTTP::Post.new('/Rest/Content/Mailings/v1/query/' + accountId)
-    #req = Net::HTTP::Post.new('/Rest/Reports/v1/mailings/' + accountId + '/' + '846245')
-
-    #filterXML = '<ListFilter></ListFilter>'
-    filterXML = '<MailingReportFilter><MaxResults>1</MaxResults></MailingReportFilter>'
-    #filterXML = '<MailingFilter><MaxResults>10</MaxResults></MailingFilter>'
-    #filterXML = '<MailingReport></MailingReport>'
-    
-    req.basic_auth 'SENDOFFE\\admin', 'Obl1skc3p0!'
+    req.basic_auth 'SENDOFFE\admin', 'Obl1skc3p0!'
     req["Content-Type"] = "text/xml"
-    req["Content-Length"] = "1020"
-    #req.set_form_data({"users[login]" => "quentin"})//example
-    
-    #serviceResponse = http.request(req, filterXML)
-    serviceResponse = http.request(req)
-    
-    #res = Net::HTTP.new('services.reachmail.net',443).start {|http| http.request(req) }
-    #case serviceResponse
-    #when Net::HTTPSuccess, Net::HTTPRedirection
-      #@response = serviceResponse.body
-    #else
-      #@response = serviceResponse.error!
-    #end
-    
-    #puts serviceResponse.body
-
-    @response = serviceResponse.body
-    #doc = REXML::Document.new(serviceResponse.body)
-    
-    #@response = Array.new
-#=end     
+    resp = http.request(req, filter)
+    #@response = resp.body # debug
+    doc = REXML::Document.new(resp.body)
+ 
+   @response = Array.new
+   
+   case report
+      when "contacts"
+        doc.elements.each("Lists/List") { |element| 
+          @response << element.elements["CreateDate"].get_text.to_s + "," + element.elements["Name"].get_text.to_s
+        }
+      when "mailings"
+        doc.elements.each("MailingReports/MailingReport") { |element| 
+          @response << element.elements["Created"].get_text.to_s + ", " + element.elements["MailingId"].get_text.to_s + ", " + element.elements["Lists/MailingListReport/ListName"].get_text.to_s
+        }
+      when "content"
+        
+      when "mailing"
+        
+    end
+   
+   
+   
+   
 =begin    
     doc.elements.each("Lists/List") { |element| 
+    #doc.elements.each("MailingReports/MailingReport") { |element| 
         #element.attributes.each {|name, value| @response <<  "{" + value.gsub(/\s/, '') + " => '" + element.get_text.to_s + "'}" }
         #element.attributes.each {|name, value| @response <<  element.get_text.to_s }
         #element.attributes.each {|name, value| hxml[ element.get_text.to_s ] }
         #@response << element.elements["Field"].elements["Name"].text unless element.elements["Field"].elements["Name"].nil?
         #@response << element.elements["Fields"].elements["Name"].text
         
-        @response << element.elements["CreateDate"].get_text.to_s + "," + element.elements["Name"].get_text.to_s
+        #@response << element.elements["Created"].get_text.to_s + ", " + element.elements["MailingId"].get_text.to_s + ", " + element.elements["Lists/MailingListReport/ListName"].get_text.to_s
         @response << element.elements["CreateDate"].get_text.to_s + "," + element.elements["Name"].get_text.to_s
       }
+=end    
+
     
-=end
-    
-    #@response = []
-    #for list in doc.elements.to_a("//Lists/List/Fields[@type='regular']")
-        #@response « list.elements['regular-body'].text
-    #end
-    
-    
-    #@response = doc.get_elements('//Fields')
-  
+ 
   
     respond_to do |format|
       format.html # new.html.erb
